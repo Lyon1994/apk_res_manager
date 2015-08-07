6568fccd2b873b8841829a3917fda137feea3e40
Lyon Yan <765211630@qq.com>
2015-08-07 17:59:37 +0800

    临时提交

--- src/org/lyon_yan/apk_res_manager/Init.java
+++ src/org/lyon_yan/apk_res_manager/Init.java
@@ -7,6 +7,7 @@ import org.lyon_yan.JBCS_framework.AppCompatActivity;
 import org.lyon_yan.JBCS_framework.AppInit;
 import org.lyon_yan.JBCS_framework.config.R;
 import org.lyon_yan.JBCS_framework.listener.JavaScriptListerner;
+import org.lyon_yan.JBCS_framework.listener.JavaScriptLoadWorkerStateListener;
 import org.lyon_yan.JBCS_framework.view.AppCompatView;
 
 public class Init extends AppInit {
@@ -16,9 +17,13 @@ public class Init extends AppInit {
 		/**
 		 * 初始化配置
 		 */
-		R.image.icon_png=R.path.resource_path+"img/icon.png";
-		R.page.index=R.path.resource_path+"install.html";
-		R.value.stageTitle="Wifimax\tOEM生成器";
+		R.image.icon_png = R.path.resource_path + "img/icon.png";
+		R.dimens.minWidth = 600;
+		R.dimens.minHeight = 800;
+		R.dimens.prefWidth = 600;
+		R.dimens.prefHeight = 800;
+		R.page.index = R.path.resource_path + "index.html";
+		R.value.stageTitle = "Wifimax\tOEM生成器";
 		launch(args);
 	}
 
@@ -26,13 +31,55 @@ public class Init extends AppInit {
 	protected Pane onPreCreate(Stage arg0) {
 		// TODO Auto-generated method stub
 		return new AppCompatActivity() {
-			
+
 			@Override
 			protected void onRegistJavaScriptListener(JavaScriptListerner arg0) {
 				// TODO Auto-generated method stub
-				
+				arg0.setJavaScriptLoadWorkerStateListener(new JavaScriptLoadWorkerStateListener() {
+
+					@Override
+					public void onSuccessedListener() {
+						// TODO Auto-generated method stub
+						System.err.println("onSuccessedListener");
+					}
+
+					@Override
+					public void onScheduledListener() {
+						// TODO Auto-generated method stub
+
+						System.err.println("onScheduledListener");
+					}
+
+					@Override
+					public void onRunningListener() {
+						// TODO Auto-generated method stub
+
+						System.err.println("onRunningListener");
+					}
+
+					@Override
+					public void onReadyListener() {
+						// TODO Auto-generated method stub
+
+						System.err.println("onReadyListener");
+					}
+
+					@Override
+					public void onFailedListener() {
+						// TODO Auto-generated method stub
+
+						System.err.println("onFailedListener");
+					}
+
+					@Override
+					public void onCancelledListener() {
+						// TODO Auto-generated method stub
+
+						System.err.println("onCancelledListener");
+					}
+				});
 			}
-			
+
 			@Override
 			protected AppCompatView onCreate(AppCompatView arg0) {
 				// TODO Auto-generated method stub
@@ -40,5 +87,5 @@ public class Init extends AppInit {
 			}
 		};
 	}
-	
+
 }
